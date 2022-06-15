dcd5b7573b4a03f1aa377384af796fca01b26fec
Tyler Preston <tpreston96@gmail.com>
2022-06-15 18:44:28 -0500

    Select By ID

---
+++ SQL/Basic_Selects/Select_By_ID.sql
@@ -0,0 +1,6 @@
+-- Prompt: https://www.hackerrank.com/challenges/select-by-id/problem
+
+-- Answer: https://www.hackerrank.com/challenges/select-by-id/submissions/code/274440412
+
+SELECT * FROM CITY
+WHERE ID = 1661;
