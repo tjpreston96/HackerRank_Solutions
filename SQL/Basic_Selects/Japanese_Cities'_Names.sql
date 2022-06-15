8f46e21626c94f58bf10b80065ed3f268ba38923
Tyler Preston <tpreston96@gmail.com>
2022-06-15 18:45:07 -0500

    Japanese Cities' Names

---
+++ SQL/Basic_Selects/Japanese_Cities'_Names.sql
@@ -0,0 +1,6 @@
+-- Prompt: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
+-- Answer: https://www.hackerrank.com/challenges/japanese-cities-name/submissions/code/274440544
+
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE='JPN'
