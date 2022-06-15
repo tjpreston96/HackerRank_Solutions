5939db13d7e7869417fa190d99eaac72668e0c79
Tyler Preston <tpreston96@gmail.com>
2022-06-15 18:44:48 -0500

    Japanese Cities' Attributes

---
+++ SQL/Basic_Selects/Japanese_Cities'_Attributes.sql
@@ -0,0 +1,6 @@
+-- Prompt: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+-- Answer: https://www.hackerrank.com/challenges/japanese-cities-attributes/submissions/code/274440465
+
+SELECT * FROM CITY
+WHERE COUNTRYCODE='JPN'
