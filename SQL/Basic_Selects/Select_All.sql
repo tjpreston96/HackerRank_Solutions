fc0ebb4a84099b244f6d3c436cb8d02f5bcf968b
Tyler Preston <tpreston96@gmail.com>
2022-06-15 18:44:15 -0500

    Select All

---
+++ SQL/Basic_Selects/Select_All.sql
@@ -0,0 +1,5 @@
+-- Prompt: https://www.hackerrank.com/challenges/select-all-sql/problem
+
+-- Answer: https://www.hackerrank.com/challenges/select-all-sql/submissions/code/274440356
+
+SELECT * FROM CITY;
