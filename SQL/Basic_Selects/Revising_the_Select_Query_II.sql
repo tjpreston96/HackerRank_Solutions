75e0b3330e0432f5ca39901352d13e4a291dfd97
Tyler Preston <tpreston96@gmail.com>
2022-06-15 18:44:02 -0500

    Revising the Select Query II

---
+++ SQL/Basic_Selects/Revising_the_Select_Query_II.sql
@@ -0,0 +1,8 @@
+-- Prompt: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+-- Answer: https://www.hackerrank.com/challenges/revising-the-select-query-2/submissions/code/274440306
+
+SELECT NAME 
+FROM CITY 
+WHERE COUNTRYCODE='USA' 
+AND POPULATION > 120000;
