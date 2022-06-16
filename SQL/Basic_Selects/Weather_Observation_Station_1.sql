48af06ffd133a7b2c743c02717ac01dc4eabe14a
Tyler Preston <tpreston96@gmail.com>
2022-06-15 20:20:15 -0500

    Weather Observation Station 1

---
+++ SQL/Basic_Selects/Weather_Observation_Station_1.sql
@@ -0,0 +1,6 @@
+-- Prompt: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+-- Answer: https://www.hackerrank.com/challenges/weather-observation-station-1/submissions/code/274440677
+
+SELECT CITY, STATE 
+FROM STATION;
