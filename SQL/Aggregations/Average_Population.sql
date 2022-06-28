6655566cfc40ef182961fcb9e6b93c2242799909
Tyler Preston <tpreston96@gmail.com>
2022-06-27 23:58:00 -0500

    Average Population

---
+++ SQL/Aggregations/Average_Population.sql
@@ -0,0 +1,6 @@
+-- Prompt: https://www.hackerrank.com/challenges/average-population/problem
+
+-- Answer:  https://www.hackerrank.com/challenges/average-population/submissions/code/276653564
+
+SELECT ROUND(AVG(POPULATION))
+FROM CITY;
