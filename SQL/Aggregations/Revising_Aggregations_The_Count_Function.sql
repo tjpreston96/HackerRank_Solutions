66fea9904341965adb7498f052e9a8d123460080
Tyler Preston <tpreston96@gmail.com>
2022-06-27 23:28:05 -0500

    Revising Aggregations - The Count Function

---
+++ SQL/Aggregations/Revising_Aggregations_The_Count_Function.sql
@@ -0,0 +1,7 @@
+-- Prompt: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+
+-- Answer: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/submissions/code/276649776
+
+SELECT COUNT(POPULATION)
+FROM CITY
+WHERE POPULATION > 100000;
