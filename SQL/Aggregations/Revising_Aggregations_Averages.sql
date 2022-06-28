0d7c2070ae36bd59b1e165ebd66f82f98a9aeaf3
Tyler Preston <tpreston96@gmail.com>
2022-06-27 23:47:23 -0500

    Revising Aggregations - Averages

---
+++ SQL/Aggregations/Revising_Aggregations_Averages.sql
@@ -0,0 +1,7 @@
+-- Prompt: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+-- Answer: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/submissions/code/276652486
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
