0b0bb69db3e133b4ceb746444905736717cae0cc
Tyler Preston <tpreston96@gmail.com>
2022-06-28 00:25:37 -0500

    Population Density Difference

---
+++ SQL/Aggregations/Population_Density_Difference.sql
@@ -0,0 +1,7 @@
+-- Prompt: https://www.hackerrank.com/challenges/population-density-difference/problem
+
+-- Answer: https://www.hackerrank.com/challenges/population-density-difference/submissions/code/276655601
+
+SELECT MAX(POPULATION) - MIN(POPULATION)
+FROM CITY;
+
