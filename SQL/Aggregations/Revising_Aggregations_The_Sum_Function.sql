5d714d844df179f1acc1e01df8bf52f3ce9ed5e5
Tyler Preston <tpreston96@gmail.com>
2022-07-08 23:45:28 -0500

    Revising Aggregations - The Sum Function

---
+++ SQL/Aggregations/Revising_Aggregations_The_Sum_Function.sql
@@ -0,0 +1,7 @@
+-- Prompt: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+-- Answer: https://www.hackerrank.com/challenges/revising-aggregations-sum/submissions/code/278396861
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
