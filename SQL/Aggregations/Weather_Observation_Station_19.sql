a4d2f2218c49a64e8cc687d3889d5c5357bb09bd
Tyler Preston <tpreston96@gmail.com>
2022-08-09 21:00:24 -0500

    Weather Observation Station 19

---
+++ SQL/Aggregations/Weather_Observation_Station_19.sql
@@ -0,0 +1,11 @@
+-- Prompt: https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+
+-- Answer: https://www.hackerrank.com/challenges/weather-observation-station-19/submissions/code/283945714
+
+SELECT
+    ROUND(SQRT(
+        POWER(MAX(LAT_N)  - MIN(LAT_N),  2)
+      + POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+    ), 4)
+FROM 
+    STATION;
